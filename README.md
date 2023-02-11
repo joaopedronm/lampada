0a2af3b2cdbaa03862dffec6a38f5b6a84b28388
joaopedronm <joaopedron253@gmail.com>
2023-02-10 21:00:54 -0300

    adicionando o projeto

---
+++ README.md
@@ -0,0 +1,4 @@
+Este é um projeto simples onde se imprime na tela uma lâmpada apagada e o usuário tem a opção
+de acendê-la, apagá-la, quebrá-la e/ou consertá-la.
+
+A lógica por trás dessas ações foram feitas em JavaScript.
---
+++ script.js
@@ -0,0 +1,44 @@
+const turnOn = document.getElementById('turnOn')
+const turnOff = document.getElementById('turnOff')
+const lamp = document.getElementById('lamp')
+const btnFix = document.getElementById('fix')
+
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1
+}
+
+function lampOn() {
+    if(!isLampBroken()) {
+        lamp.src = 'ligada.jpg'
+    }
+
+}
+
+function lampOff() {
+    if(!isLampBroken()) {
+        lamp.src = 'desligada.jpg'
+    }
+}
+
+function lampBroken() {
+    lamp.src = 'quebrada.jpg'
+    if(btnFix.style.display === 'none') {
+        btnFix.style.display = 'block'
+    }
+}
+
+function fixLamp() {
+    if(isLampBroken()) {
+        lamp.src = 'desligada.jpg'
+        btnFix.style.display = 'none'
+    }
+}
+
+turnOn.addEventListener('click', lampOn)
+turnOff.addEventListener('click', lampOff)
+// lamp.addEventListener('mouseover', lampOn)
+// lamp.addEventListener('mouseleave', lampOff)
+
+lamp.addEventListener('dblclick', lampBroken)
+
+fix.addEventListener('click', fixLamp)
