6ba820eaab191767257b65c66b3b985df5eb649b
joaopedronm <joaopedron253@gmail.com>
2023-02-28 10:46:33 -0300

    atualizações

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const turnOn = document.getElementById('turnOn')
 const turnOff = document.getElementById('turnOff')
 const lamp = document.getElementById('lamp')
 const btnFix = document.getElementById('fix')
+const orientacao = document.getElementById('orientacao')
 
 function isLampBroken() {
     return lamp.src.indexOf('quebrada') > -1
@@ -9,29 +10,36 @@ function isLampBroken() {
 
 function lampOn() {
     if(!isLampBroken()) {
-        lamp.src = '/img/ligada.jpg'
+        lamp.src = './img/ligada.jpg'
     }
 
 }
 
 function lampOff() {
     if(!isLampBroken()) {
-        lamp.src = '/img/desligada.jpg'
+        lamp.src = './img/desligada.jpg'
     }
 }
 
 function lampBroken() {
-    lamp.src = '/img/quebrada.jpg'
+    lamp.src = './img/quebrada.jpg'
     if(btnFix.style.display === 'none') {
         btnFix.style.display = 'block'
     }
+    orientacao.style.display = 'none'
+    turnOn.style.display = 'none'
+    turnOff.style.display = 'none'
+
 }
 
 function fixLamp() {
     if(isLampBroken()) {
-        lamp.src = '/img/desligada.jpg'
+        lamp.src = './img/desligada.jpg'
         btnFix.style.display = 'none'
     }
+    orientacao.style.display = 'inline'
+    turnOn.style.display = 'block'
+    turnOff.style.display = 'block'
 }
 
 turnOn.addEventListener('click', lampOn)
